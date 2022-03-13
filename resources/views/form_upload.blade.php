10cea5c12d6534fb17a1a84743bb7804f3f70d9f
Rani Dewi Aristhania <rani_aristhania_23rpl@student.smktelkom-mlg.sch.id>
2022-03-13 19:09:18 +0700

    first commit

--- resources/views/form_upload.blade.php
+++ resources/views/form_upload.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Upload Data Mahasiswa</title>
+    <title>Upload Data Mahasiswa </title>
     <style>
     * {
         box-sizing: border-box;
